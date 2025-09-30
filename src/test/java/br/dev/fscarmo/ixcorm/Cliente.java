6806801fc0b0b31bb4c9350365ac68ef67c812b8
Felipe Sousa <sousa.felipe@outlook.com>
2025-09-29 23:06:41 -0300

    fix: classes de teste separadas do código principal

--- src/test/java/br/dev/fscarmo/ixcorm/Cliente.java
+++ src/test/java/br/dev/fscarmo/ixcorm/Cliente.java
@@ -1,7 +1,11 @@
 package br.dev.fscarmo.ixcorm;
 
 
+import com.google.gson.JsonElement;
+
+
 public class Cliente extends IxcOrm {
+
 
     public Cliente() {
         super("cliente");
@@ -9,5 +13,42 @@ public class Cliente extends IxcOrm {
 
     public static Cliente newCliente() {
         return new Cliente();
+    }
+
+
+    public static class Record extends IxcRecord {
+
+        private String razao;
+        private String cnpj_cpf;
+
+        public Record(JsonElement element) {
+            super(element);
+        }
+
+        @Override
+        protected void map() {
+            mapCnpjCpf();
+            mapRazao();
+        }
+
+        public String getCnpjCpf() {
+            return cnpj_cpf;
+        }
+
+        public String getRazao() {
+            return razao;
+        }
+
+        private void mapCnpjCpf() {
+            if (elementHasProperty("cnpj_cpf")) {
+                cnpj_cpf = get("cnpj_cpf").getAsString();
+            }
+        }
+
+        private void mapRazao() {
+            if (elementHasProperty("razao")) {
+                razao = get("razao").getAsString();
+            }
+        }
     }
 }
